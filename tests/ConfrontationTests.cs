1003d839fec3f900932b83b9acde2fa0d39f0d50
James Birnie <jamesbirnie@Jamess-MacBook-Pro-3.local>
2020-02-05 11:52:12 +0000

    split the theory in two as it made no sense as it was
    James Birie

--- tests/ConfrontationTests.cs
+++ tests/ConfrontationTests.cs
@@ -3,14 +3,24 @@ namespace SelfishMeme
 {
     public class ConfrontationTests
     {
-        [Theory]
-        [InlineData(BirdType.Hawk, BirdType.Dove)]
-        public void HawkAlwaysBeatsDoveByWalkover(BirdType firstBirdType, BirdType secondBirdType)
+        [Fact]
+        public void HawkAlwaysBeatsDoveByWalkover()
         {
-            var hawk = new Bird(firstBirdType);
-            var dove = new Bird(secondBirdType);
+            var hawk = new Bird(BirdType.Hawk);
+            var dove = new Bird(BirdType.Dove);
             var confrontation = new Confrontation();
             confrontation.Resolve(hawk, dove);
+            Assert.Equal(50, hawk.LifePoints);
+            Assert.Equal(0, dove.LifePoints);
+        }
+
+         [Fact]
+        public void DoveAlwaysLosesToHawkByWalkover()
+        {
+            var hawk = new Bird(BirdType.Hawk);
+            var dove = new Bird(BirdType.Dove);
+            var confrontation = new Confrontation();
+            confrontation.Resolve(dove, hawk);
             Assert.Equal(50, hawk.LifePoints);
             Assert.Equal(0, dove.LifePoints);
         }
